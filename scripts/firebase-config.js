d33d8a77cc943710d71bb95b4995fc6cf9c0b15a
jhux12 <79742669+jhux12@users.noreply.github.com>
2025-05-07 02:11:14 -0400

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,1 +1,17 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyRm6dWH-fAmfWy83zLTrPFVi9Ny8gyxE",
+  authDomain: "cases-e5b4e.firebaseapp.com",
+  databaseURL: "https://cases-e5b4e-default-rtdb.firebaseio.com",
+  projectId: "cases-e5b4e",
+  storageBucket: "cases-e5b4e.appspot.com",
+  messagingSenderId: "22502548396",
+  appId: "1:22502548396:web:aac335672c21f07524d009"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export reference to Firebase Database
+const db = firebase.database();
 
