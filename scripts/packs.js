b04b199753aefaae31878514000a844dea2c0973
jhux12 <79742669+jhux12@users.noreply.github.com>
2025-05-09 17:36:59 -0400

    Update packs.js

--- scripts/packs.js
+++ scripts/packs.js
@@ -18,7 +18,7 @@ function loadCases() {
           <img src="${data.image}" class="case-card-img mb-2">
           <h3 class="mt-2 font-semibold text-white">${data.name}</h3>
           <button class="mt-2 w-full py-2 bg-gradient-to-r from-purple-600 to-pink-500 rounded open-case glow-button enhanced-glow flex justify-center items-center gap-2 text-white font-semibold" data-id="${child.key}">
-            Open for ${data.price || '0'} 
+            Open for ${(parseFloat(data.price) || 0).toLocaleString()}
             <img src="https://cdn-icons-png.flaticon.com/128/6369/6369589.png" alt="Coin" class="w-4 h-4 inline-block">
           </button>
         </div>`;
