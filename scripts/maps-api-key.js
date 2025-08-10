26f7e26918223dcdd8e85f85b3cbe13d18f6c060
jhux12 <79742669+jhux12@users.noreply.github.com>
2025-08-09 21:49:35 -0400

    Update maps-api-key.js

--- scripts/maps-api-key.js
+++ scripts/maps-api-key.js
@@ -1,3 +1,3 @@
 // Set your Google Maps API key here. Leave blank to disable autocomplete.
 // Example: window.GOOGLE_MAPS_API_KEY = "YOUR_API_KEY_HERE";
-window.GOOGLE_MAPS_API_KEY = "";
+window.GOOGLE_MAPS_API_KEY = "AIzaSyBlvbjMQrnGczRRZX9hDze4MqlU4etkuPw";
