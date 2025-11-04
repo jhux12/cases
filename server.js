f2d92ad591a45e684272b2ec290598942a2a18bd
jhux12 <79742669+jhux12@users.noreply.github.com>
2025-11-04 15:03:40 -0500

    Add Express server for container deployments

---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+
+const spinHandler = require('./functions/src/spin');
+const runBattleHandler = require('./functions/src/run-battle');
+
+const app = express();
+
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+function wrap(handler) {
+  return (req, res, next) => {
+    Promise.resolve(handler(req, res)).catch(next);
+  };
+}
+
+app.options('/api/spin', wrap(spinHandler));
+app.post('/api/spin', wrap(spinHandler));
+
+app.options('/api/run-battle', wrap(runBattleHandler));
+app.get('/api/run-battle', wrap(runBattleHandler));
+
+const staticRoot = path.join(__dirname);
+const staticMiddleware = express.static(staticRoot, {
+  dotfiles: 'ignore',
+  fallthrough: true,
+  index: false
+});
+
+app.use((req, res, next) => {
+  if (req.path.startsWith('/api/')) {
+    return next();
+  }
+  return staticMiddleware(req, res, next);
+});
+
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api/')) {
+    return next();
+  }
+  res.sendFile(path.join(staticRoot, 'index.html'));
+});
+
+const port = Number(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
